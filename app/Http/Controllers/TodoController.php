f50e106b291e26190b052f0ddeeee92af99abc5c
RojanShrestha77 <shresrozan555@gmail.com>
2026-06-11 19:09:49 +0545

    error handling

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -11,7 +11,17 @@ class TodoController extends Controller
     public function index()
     {
         return response()->json(Todo::all(), 200);
-    }
+    }  #returns all the todos
+
+    public function show($id)
+    {
+        try {
+            $todo = Todo::findOrFail($id);
+            return response() ->json($todo, 200);
+        } catch (ModelNotFoundException $e) {
+            return response() -> json(['message' => 'Todo not found'], 404);
+        }
+    } #returns only the todo that matches the id
 
     // POST/ todos - create a todo
     public function store(Request $request)
@@ -27,22 +37,32 @@ class TodoController extends Controller
     // PUT /todos/{id} - update a todo
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'title' => 'sometimes|string|max:255',  #sometimes means : Only validate this field if it is present in the request
-            'completed' => 'sometimes|boolean',
-        ]);
-
-        $todo = Todo::findOrFail($id);
-        $todo->update($request->all());
-        return $todo;
+        try {
+            $request->validate([
+                'title' => 'sometimes|string|max:255',  #sometimes means : Only validate this field if it is present in the request
+                'completed' => 'sometimes|boolean',
+            ]);
+    
+            $todo = Todo::findOrFail($id);
+            $todo->update($request->all());
+            return response() ->json($todo, 200);
+        }     catch(ModelNotFoundException $e) {
+            return response()->json(['message' => 'Todo not found'], 404)
+        }
+        
     }
 
     // Delete /todos/{id} - delete a todo
     public function destroy($id)
     {
-        $todo = Todo::findOrFail($id);
-        $todo->delete();
-        return response()->json(['message' => 'Todo deleted'], 200);
+        try {
+
+            $todo = Todo::findOrFail($id);
+            $todo->delete();
+            return response()->json(['message' => 'Todo deleted'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response() -> json(['message' => 'Todo not found'], 404)
+        }
     }
 
 }
